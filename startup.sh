d4b15926d922cf48ad85451c9e7581b7affbbfa5
Eric Wittmann <eric.wittmann@gmail.com>
2019-12-10 16:33:10 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ -d $HOME/anaconda]
-then
+if [ -d $HOME/anaconda]; then
   wget https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
   bash Anaconda3-2019.10-Linux-x86_64.sh -b -p $HOME/anaconda
   $HOME/anaconda/bin/pip install qiskit
+  $HOME/anaconda/bin/jupyter notebook --generate-config
   echo 'Enter a password for Jupyter Notebook'
   $HOME/anaconda/bin/jupyter notebook password
 fi
