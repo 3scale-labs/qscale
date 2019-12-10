7ad276f347d0b00fe4c6730a8a04cf9b2469cc77
Eric Wittmann <eric.wittmann@gmail.com>
2019-12-10 18:00:23 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,7 +5,10 @@ if [ ! -d $HOME/anaconda ]; then
   bash Anaconda3-2019.10-Linux-x86_64.sh -b -p $HOME/anaconda
   $HOME/anaconda/bin/pip install qiskit
   $HOME/anaconda/bin/jupyter notebook --generate-config
+  echo '-------------------------------------'
   echo 'Enter a password for Jupyter Notebook'
+  echo '-------------------------------------'
   $HOME/anaconda/bin/jupyter notebook password
+  echo "c.NotebookApp.allow_origin = '*'" >> ~/.jupyter/jupyter_notebook_config.py
 fi
 $HOME/anaconda/bin/jupyter notebook
